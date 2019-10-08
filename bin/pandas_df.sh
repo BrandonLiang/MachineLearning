779344ddbf8fbe84bcbd67d56db29557ef279881
BrandonLiang <xl2891@columbia.edu>
2019-10-08 17:21:18 -0400

    added double quotes in bin to ensure fault tolerance with space in filepath

--- bin/pandas_df.sh
+++ bin/pandas_df.sh
@@ -5,8 +5,8 @@ APP_HOME=$SCRIPT_DIR/..
 #SRC=${APP_HOME}/src
 SRC=/Users/brandonliang/src/MachineLearning-master/src
 
-FILEPATH=$1
+FILEPATH="$1"
 ROWS=$2
 WIDTH=$3
 
-python ${SRC}/pandas_df.py $FILEPATH $ROWS $WIDTH
+python ${SRC}/pandas_df.py "$FILEPATH" $ROWS $WIDTH
